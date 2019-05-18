dee04ecb842eb5ea025ed7992b9ce2b20bb94ae9
Tiger 1 <polunindima2002@gmail.com>
2019-05-18 19:41:18 +0300

    v.0.1.0

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from bs4 import BeautifulSoup
 from telebot import TeleBot, types
 from rules import Rule, r
 from urllib.parse import unquote
+import googleapiclient.discovery as ds
+import google_auth_oauthlib.flow as fl
 
 creator = 268486177
 bot = TeleBot('616926239:AAG4j5if1vFODOdcqeM9ID8PFCfRyu95kaM')
@@ -43,7 +45,39 @@ def google(m):
     if not is_ans:
         bot.send_message(m.chat.id, 'Ответов на ваш запрос нет')
         return
-    bot.send_message(m.chat.id, text, parse_mode='HTML')
+    bot.send_message(m.chat.id, text, parse_mode='HTML', disable_web_page_preview=True)
+
+
+scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+# flow = fl.InstalledAppFlow.from_client_secrets_file('telegram-bot-12ab15b21c08.json', scopes)
+# credentials = flow.run_console()
+youtube = ds.build('youtube', 'v3', developerKey='AIzaSyDDvaLsMyPuwr6hWHmIeOMxwc52W4gyrpM')
+
+
+@bot.message_handler(commands=['y'])
+def search_youtube(m):
+    text = m.text.split(' ', maxsplit=1)
+    if len(text) != 2:
+        bot.send_message(m.chat.id, '/y <запрос>')
+        return
+    request = youtube.search().list(
+        part='snippet',
+        maxResults=5,
+        q=text[1]
+    )
+    response = request.execute()
+    text = ''
+    print(response)
+    if response['pageInfo']['totalResults'] == 0:
+        bot.send_message(m.chat.id, 'По вашему запросу ничего не найдено :(')
+        return 
+    for item in response['items']:
+        try:
+            text += '<a href="http://youtu.be/{}">{}</a>\n'.format(item['id']['videoId'], item['snippet']['title'])
+        except KeyError as e:
+            text += '<a href="http://youtu.be/{}">{}</a>\n'.format(item['id']['channelId'], item['snippet']['title'])
+        text += '{}\n\n'.format(item['snippet']['description'])
+    bot.send_message(m.chat.id, text, parse_mode='HTML', disable_web_page_preview=True)
 
 
 @bot.message_handler(content_types=['text'])
